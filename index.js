e9670d74b5f132a2ecf387ffb35346e9d73e69f6
Jose Salazar <solisjose466@gmail.com>
2019-12-08 17:57:23 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//Get
+var contact = document.getElementById("contact");
+var box3 = document.getElementById("box3");
+//Events
+contact.addEventListener("click", () => {
+  box3.scrollIntoView();
+});
+//functions
