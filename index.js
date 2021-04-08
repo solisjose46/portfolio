9e6c28c4700d4d72613570956fb45ad95c5ad8e0
Jose Salazar <solisjose46@pop-os.localdomain>
2021-04-08 00:08:00 -0700

    new look

--- index.js
+++
@@ -1,8 +0,0 @@
-//Get
-var contact = document.getElementById("contact");
-var box3 = document.getElementById("box3");
-//Events
-contact.addEventListener("click", () => {
-  box3.scrollIntoView();
-});
-//functions
